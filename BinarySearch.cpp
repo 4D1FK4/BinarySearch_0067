82ebd7807bd2fd439648eb991977a738476543d2
4D1FK4 <adifkapersonal0812@gmail.com>
2026-04-16 10:46:02 +0700

    Element after being sorted (ascending)

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -47,5 +47,13 @@ void bubbleSortArray()
                 element[j + 1] = temp;
             }
         }
-    }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
+void display()
+{
+    cout << "n\===========================================\n";
+    cout << "    Elemen Array Setelah Diurutkan (Asc)     \n";
+    cout << "===========================================\n";
 }
